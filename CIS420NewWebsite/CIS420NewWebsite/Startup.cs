054bb84be318bde56ae2304962e2bd34a2b3f055
Joseph M Dawson <jmdaws02@cardmail.louisville.edu>
2017-09-18 15:33:41 -0400

    initial Commit

---
+++ CIS420NewWebsite/CIS420NewWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIS420NewWebsite.Startup))]
+namespace CIS420NewWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
