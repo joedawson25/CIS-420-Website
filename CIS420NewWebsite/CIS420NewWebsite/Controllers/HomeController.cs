372d0f27e641834b19900f22bfa9afe0e419cf64
Zachary J Endicott <zjendi01@louisville.edu>
2017-10-01 15:28:33 -0400

    Added content to News page

--- CIS420NewWebsite/CIS420NewWebsite/Controllers/HomeController.cs
+++ CIS420NewWebsite/CIS420NewWebsite/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using CIS420NewWebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace CIS420NewWebsite.Controllers
 {
@@ -34,9 +38,25 @@ namespace CIS420NewWebsite.Controllers
         {
             return View();
         }
-        public ActionResult News()
+        //News controller updated. Pull feed from HBPA National Site
+        public async Task<ActionResult> News()
         {
-            return View();
+            // https://stackoverflow.com/questions/11194757/consuming-wordpress-rss-feed-in-asp-net Site for reference
+
+            // GetAsync is a promise to provide something; 
+            // keyword "await" tells the application to await the data before moving on.
+            var response = await new HttpClient().GetAsync("https://nationalhbpa.com/category/kentucky-hbpa/feed/");
+            var content = await response.Content.ReadAsStringAsync(); // Convert from bits to text
+            var posts = XDocument.Parse(content).Descendants("item")
+                .Select(post => new Article
+                 {
+                     Title = post.Element("title").Value,
+                     Link = post.Element("link").Value,
+                     PublicationDate = DateTime.Parse(post.Element("pubDate").Value)
+                 });
+
+            return View(posts); // Becomes available to the view.
+
         }
         public ActionResult Documents()
         {
---
+++ CIS420NewWebsite/CIS420NewWebsite/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS420NewWebsite.Models
+{
+    // Create class to help MVC and VS and make our lives easier. Allows us to use as an IEnumerable type in the model
+    // for the News Model. Allows for autocomplete. Holds the projected view from the WordPress feed for the controller.
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public DateTime PublicationDate { get; set; } 
+    }
+}
