5abcc786d9fcbb5eb1e412a0c4557ebea633d202
Kimberly R Roeten <krroet01@louisville.edu>
2017-10-25 09:29:13 -0400

    photo model added

---
+++ CIS420NewWebsite/CIS420NewWebsite/Models/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS420NewWebsite.Models
+{
+    public class Photo
+    {
+        public string ID { get; set; }
+        public string PhotoName { get; set; }
+        public string Description { get; set; }
+        public int ContentLength { get; set; }
+        public string ContectType { get; set; }
+        public DateTime DateUploaded { get; set; }
+        public Member UploadedBy { get; set; }
+    }
+}
